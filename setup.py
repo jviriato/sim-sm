31956899e748274a871001ebc4fb0a8d591d39f3
jviriato <josevviriato@gmail.com>
2019-01-11 01:39:01 -0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sim-sm',
+    version='0.1',
+    scripts=['sim-sm'],
+    url='https://github.com/jviriato/sim-sm/',
+    author='José Victor Viriato',
+    license='MIT',
+    author_email='josevviriato@gmail.com',
+    install_requires=[
+        'terminaltables'
+    ],
+    keywords=['CLI', 'bus', 'python']
+)
