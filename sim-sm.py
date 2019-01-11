e9f53c8d1f0a547342d562060508a5626dda6695
jviriato <josevviriato@gmail.com>
2019-01-11 00:29:56 -0200

    printa tabela =)

--- sim-sm.py
+++ sim-sm.py
@@ -2,6 +2,8 @@
 
 import requests, json
 from pprint import pprint
+from datetime import datetime
+from terminaltables import AsciiTable
 
 # Constantes utilizadas no site SIM SM 
 URL = 'http://www.simsm.com.br/ajax/buscarHorariosRotasNovo/'
@@ -21,10 +23,32 @@ DOMINGO         = 2
 
 def getData(sentido, periodo):
     print('Fazendo a requisição...')
-    r = requests.post(URL, data={'sentido': sentido, 'periodo': periodo})
-    # print(r.text)
-    pp_json = json.loads(r.text)
-    pprint(pp_json)
+    # r = requests.post(URL, data={'sentido': sentido, 'periodo': periodo})
+    # data = json.loads(r.text)
+
+    data = json.load(open('via.json', 'r'))
+
+    data_list = []
+    pprint('Atualizado em: ' + data['atualizado'])
+    for i in range(len(data['dados'])):
+        data_list.append([data['dados'][i]['obs'], data['dados'][i]['hora']])
+    
+    printTable(data_list)
+
+def printTable(data_list):
+    table_data = [
+        ['Via', 'Horário']
+    ]
+    for i in data_list:
+        table_data.append(i)
+    # table_data.append(['oi', 'limao'])
+    print(data_list)
+
+    # table_data.append(data_list)
+    table_instance = AsciiTable(table_data, 'UFSM -> CENTRO')
+    print(table_instance.table)
+    dt = datetime.now()
+    print('Horário: ' + str(dt.hour) + ':' + str(dt.minute))
 
 def main():
     getData(UFSM_CENTRO, DIAS_UTEIS)
