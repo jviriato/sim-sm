7806ec1c79384c9f5313c5247662d51f2d309d1b
José Victor Viriato <josevviriato@gmail.com>
2019-01-12 00:55:55 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup
 
 setup(
     name='sim-sm',
+    description="Linhas de ônibus do SIM-SM no seu terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version='0.1',
     scripts=['sim-sm'],
     url='https://github.com/jviriato/sim-sm/',
     author='José Victor Viriato',
     license='MIT',
     author_email='josevviriato@gmail.com',
-    install_requires=[
-        'terminaltables'
-    ],
+    packages=setuptools.find_packages(),   
     keywords=['CLI', 'bus', 'python']
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
 )
